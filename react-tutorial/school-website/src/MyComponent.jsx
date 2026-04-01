8f5b1f04d9fe6e72719ca94fbfd5dc5348b964c3
waseem123 <waseem_6957@yahoo.com>
2026-04-01 18:58:36 +0530

    react apps

---
+++ react-tutorial/school-website/src/MyComponent.jsx
@@ -0,0 +1,8 @@
+export default function MyComponent(){
+    return(
+        <>
+            <h2>My Component</h2>
+            <p>This is My Component created for testing</p>
+        </>
+    );
+}
