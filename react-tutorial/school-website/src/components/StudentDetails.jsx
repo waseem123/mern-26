c00b65aab74d0d12091137d62079ea7a5274e17f
waseem123 <waseem_6957@yahoo.com>
2026-04-10 19:16:33 +0530

    dynamic ulr

--- react-tutorial/school-website/src/components/StudentDetails.jsx
+++ react-tutorial/school-website/src/components/StudentDetails.jsx
@@ -1,26 +1,39 @@
 import allstudents from './students-data.json';
 import { useParams } from 'react-router-dom';
+import './styles.css';
 
 export default function StudentDetails() {
     let { rollno } = useParams();
-    return (
-        <div className="container my-5">
-            <div className="row">
-                <div className="col-12">
-                    <div className="card shadow-lg">
-                        <div className="card-body">
-                            <div className="row">
-                                <img src="https://static.vecteezy.com/system/resources/thumbnails/004/607/791/small_2x/man-face-emotive-icon-smiling-male-character-in-blue-shirt-flat-illustration-isolated-on-white-happy-human-psychological-portrait-positive-emotions-user-avatar-for-app-web-design-vector.jpg" className="col-4 card-img-left"></img>
-                                <div className="col-8">
-                                    <h5>{}</h5>
-                                    <div>City</div>
-                                    <div>Marks</div>
+    let backgroundClass = (rollno % 2 == 0) ? 'text-bg-success' : 'text-bg-danger'
+    let s = allstudents.filter(obj => obj.rollNo == rollno);
+    if (s.length == 0) {
+        return (
+            <h1 className='display-1 text-danger text-center'>No Data Found for Roll No - {rollno}</h1>
+        )
+    } else {
+        return (
+            <div className="container my-5">
+                <div className="row">
+                    <div className="col-12">
+                        <div className={`card shadow-lg ${backgroundClass}`}>
+                            <div className="card-body">
+                                <div className="row">
+                                    {/* <img src="https://static.vecteezy.com/system/resources/thumbnails/004/607/791/small_2x/man-face-emotive-icon-smiling-male-character-in-blue-shirt-flat-illustration-isolated-on-white-happy-human-psychological-portrait-positive-emotions-user-avatar-for-app-web-design-vector.jpg" className="col-4 card-img-left"></img> */}
+                                    <div className='col-4 mx-2 my-2 f-letter'>
+                                        {s[0].name[0]}
+                                    </div>
+                                    <div className="col-8 mx-2 my-3  ">
+                                        <h5>{s[0].name}</h5>
+                                        <div>{s[0].city}</div>
+                                        <div>{s[0].marks}</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+
 }
