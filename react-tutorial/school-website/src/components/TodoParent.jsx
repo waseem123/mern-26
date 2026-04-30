fd592aa2510ed682f782cc58b90043d5b357109f
waseem123 <waseem_6957@yahoo.com>
2026-04-30 19:19:54 +0530

    Task Manager

--- react-tutorial/school-website/src/components/TodoParent.jsx
+++ react-tutorial/school-website/src/components/TodoParent.jsx
@@ -4,14 +4,20 @@ import Task from "./Task"
 export default function TodoParent() {
     const [allTasks, setAllTasks] = useState([])
     const [taskTitle, setTaskTitle] = useState("")
+    const [taskTime, setTaskTime] = useState("")
     const [taskCount, setTaskCount] = useState(0)
-    
+
     let taskData = allTasks.map(task =>
         <Task taskTitle={task} />
     )
     const addNewTask = () => {
-        setAllTasks(allTasks => [...allTasks, taskTitle])
-        setTaskCount(taskCount + 1)
+        if (taskTitle == "" || taskTitle.length == 0) {
+            alert("Task can not be empty")
+        } else {
+            setAllTasks(allTasks => [...allTasks, taskTitle])
+            setTaskCount(taskCount + 1)
+            setTaskTitle("")
+        }
     }
     return (
         <div className="container">
@@ -19,7 +25,8 @@ export default function TodoParent() {
                 <h2 className="text-end">Total Number of Tasks - {taskCount}</h2>
                 <hr />
                 <div>
-                    <input onChange={(e) => { setTaskTitle(e.target.value) }} type="text" placeholder="Enter your task" />
+                    <input onChange={(e) => { setTaskTitle(e.target.value) }} value={taskTitle} type="text" placeholder="Enter your task" />
+                    <input onChange={(e) => { setTaskTime(e.target.value) }} value={taskTime} type="text" placeholder="Enter task time" />
                     <button onClick={addNewTask}>Add</button>
                 </div>
 
